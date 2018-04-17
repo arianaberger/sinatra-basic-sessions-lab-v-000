c37ef6cfb2a114e9503e7738c5d9e2b2bfa40274
Ariana Berger <ariana.berger@gmail.com>
2018-04-17 20:49:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @session = session
-    @session[:item] = params["item"]
+    @session["item"] = params["item"]
 
-    erb :items
+    erb :checkout
   end
 end
